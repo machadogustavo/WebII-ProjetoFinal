101f82f82daebdb8089d298acf53aa3d3ca0c942
Gabriel Morandi de Mello <61758357+gabrielmorandi@users.noreply.github.com>
2023-03-24 23:35:42 -0300

    add mobile menu

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,31 +1,45 @@
-import React from 'react'
-
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import Logo from '../../assets/Logo.png'
-import Menu from '../../assets/Menu.svg'
+
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
 
 const Navbar = () => {
-    return (
-        <nav>
-            <div>
-                <div>
-                    <img src={Logo} alt="Logo" />
-                    <Link to={'/home'}>Print Storage</Link>
-                </div>
-                <img src={Menu} alt="Menu" className='open-menu' />
-                <ul>
-                    <li><Link to={'/home'}>Home</Link></li>
-                    <li><Link to={'/'}>Cliente</Link></li>
-                    <li><Link to={'/admin'}>Admin</Link></li>
-                    <li><Link to={'/ajuda'}>Ajuda</Link></li>
-                </ul>
-            </div>
-            <Link to={'/'} className='btn-secundary btn-border-blue'>
-                <p>Envie seu arquivo!</p>
-            </Link>
-        </nav>
-    )
+	const [showMenu, setShowMenu] = useState(false)
+
+	const toggleMenu = () => {
+		setShowMenu(!showMenu)
+	}
+
+	const closeMenu = () => {
+		setShowMenu(false)
+	}
+
+	return (
+		<nav>
+			<div>
+				<div>
+					<img src={Logo} alt="Logo" />
+					<Link to={'/home'} onClick={closeMenu}>Print Storage</Link>
+				</div>
+				{showMenu ? (
+					<RiCloseLine onClick={toggleMenu} size={32} color={"#6246ea"} />
+				) : (
+					<RiMenu3Line onClick={toggleMenu} size={32} color={"#6246ea"} className="open-menu" />
+				)}
+				<ul className={showMenu ? 'menu-active' : 'menu-hidden'}>
+					<li><Link to={'/home'} onClick={closeMenu}>Home</Link></li>
+					<li><Link to={'/'} onClick={closeMenu}>Cliente</Link></li>
+					<li><Link to={'/admin'} onClick={closeMenu}>Admin</Link></li>
+					<li><Link to={'/ajuda'} onClick={closeMenu}>Ajuda</Link></li>
+				</ul>
+			</div>
+			<Link to={'/'} className='btn-secundary btn-border-blue'>
+				<p>Envie seu arquivo!</p>
+			</Link>
+		</nav>
+	)
 }
 
 export default Navbar
